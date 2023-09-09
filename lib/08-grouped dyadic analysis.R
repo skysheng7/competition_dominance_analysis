f2b7af51c1229f4ecdabd54552bcc4fa0f8b6974
Sheng <82303914+skysheng7@users.noreply.github.com>
2023-09-08 17:38:53 -0700

    grouped dyadic analysis done

--- lib/08-grouped dyadic analysis.R
+++ lib/08-grouped dyadic analysis.R
@@ -25,3 +25,133 @@ group_density_buckets <- function(df, density_column) {
   
   return(df)
 }
+
+#' Find Dyads with >=2 Interactions in All Levels
+#'
+#' This function identifies dyads that have at least 2 interactions and are present 
+#' in all of 5 grouped feeder occupancy levels
+#'
+#' @param data A dataframe containing the dyad data.
+#' @param dyad_id_col A string specifying the name of the column containing dyad IDs.
+#' @param occupancy_col A string specifying the name of the column containing occupancy levels.
+#' 
+#' @return A vector containing dyad IDs that are present in all unique levels of the occupancy column.
+find_dyads_in_all_levels_mt2 <- function(data, dyad_id_col, occupancy_col) {
+  data <- data[which(data$total_interactions>=2),]
+  # Group the data by dyad_id and feeder_occupancy_grouped
+  grouped_data <- aggregate(data[[occupancy_col]], by = list(data[[dyad_id_col]], data[[occupancy_col]]), FUN = length)
+  
+  # Count the number of unique levels of feeder_occupancy_grouped for each dyad_id
+  dyad_counts <- aggregate(grouped_data$x, by = list(grouped_data$Group.1), FUN = length)
+  
+  # Find the total number of unique levels of feeder_occupancy_grouped
+  total_levels <- length(unique(data[[occupancy_col]]))
+  
+  # Filter the dyad_id that have counts equal to the total number of unique levels of feeder_occupancy_grouped
+  dyads_in_all_levels <- dyad_counts[dyad_counts$x == total_levels, "Group.1"]
+  
+  return(dyads_in_all_levels)
+}
+
+
+#' Calculate Change in Winning Percentage per Dyad
+#'
+#' This function computes the change in winning percentage (`win_pct`) for each unique dyad 
+#' as the feeder occupancy increases. The change is calculated relative to the winning 
+#' percentage at the lowest feeder occupancy.
+#'
+#' @param df A dataframe containing the dyad data.
+#' @param dyad_id_col A string specifying the name of the column containing dyad IDs.
+#' @param occupancy_col A string specifying the name of the column containing occupancy levels.
+#' @param win_pct_col A string specifying the name of the column containing winning percentages.
+#' 
+#' @return A dataframe with columns for the dyad ID, occupancy level, winning percentage, 
+#'         change in winning percentage, and absolute change in winning percentage.
+calculate_win_pct_change_per_dyad <- function(df, dyad_id_col, occupancy_col, win_pct_col) {
+  # Get the unique dyad_ids
+  dyad_ids <- unique(df[[dyad_id_col]])
+  
+  # Initialize a list to store the results
+  results <- list()
+  
+  # Loop over the dyad_ids
+  for(dyad_id in dyad_ids) {
+    # Get the subset of data for the current dyad_id
+    dyad_df <- df[df[[dyad_id_col]] == dyad_id, ]
+    
+    # Sort the dyad_df by feeder_occupancy_grouped
+    dyad_df <- dyad_df[order(dyad_df[[occupancy_col]]), ]
+    
+    # Get the win_pct at the lowest feeder_occupancy_grouped
+    base_win_pct <- dyad_df[[win_pct_col]][1]
+    
+    # Calculate the change in win_pct
+    dyad_df$win_pct_change <- dyad_df[[win_pct_col]] - base_win_pct
+    
+    # Calculate the absolute change in win_pct
+    dyad_df$win_pct_abs_change <- abs(dyad_df$win_pct_change)
+    
+    # Add the result to the list
+    results[[dyad_id]] <- dyad_df
+  }
+  
+  # Bind all the data frames in the list into a single data frame
+  result_df <- do.call(rbind, results)
+  
+  return(result_df)
+}
+
+
+#' Plot Positive Changes in Winning Percentage using Violin and Box Plots
+#'
+#' @param win_pct_changes2_pos A dataframe containing positive changes in winning percentages.
+positive_win_pct_change_violin_boxplot <- function(win_pct_changes2_pos) {
+  
+  # Define a color vector
+  color_vector <- viridis::viridis(length(unique(win_pct_changes2_pos$feeder_occupancy_grouped)), direction = -1)
+  
+  # Define labels
+  labels = c("(0.27, 0.43]", "(0.43, 0.6]", "(0.6, 0.77]", "(0.77, 1]")
+  
+  violin_boxplot <- ggplot(win_pct_changes2_pos, aes(x = as.factor(feeder_occupancy_grouped), y = win_pct_change, fill = as.factor(feeder_occupancy_grouped))) +
+    geom_violin(lwd = 1.5, color = NA) +
+    scale_fill_viridis_d(name = "Feeder \nOccupancy", direction = -1, guide = "none") +
+    geom_boxplot(width = 0.1, fill = "white", lwd = 1.5, outlier.shape = 19, outlier.size = 6, outlier.fill = "black") +
+    labs(x = "Feeder Occupancy", y = "Positive Changes in \nPercentage of Winning", fill = "Feeder Occupancy") +
+    theme_classic() +
+    #theme(text = element_text(size = 55), axis.text.x = element_text(size = 40, color = color_vector)) +
+    theme(text = element_text(size = 55), axis.text.x = element_text(size = 40)) +
+    scale_y_continuous(expand = expansion(mult = c(0, .1)), limits = c(0, 1)) +
+    scale_x_discrete(labels = labels)
+  
+  file_name = here("graphs/positive_win_pct_change_violin_boxplot.png")
+  ggsave(file_name, plot = violin_boxplot, width = 15, height = 13, limitsize = FALSE)
+  
+}
+
+#' Plot Negative Changes in Winning Percentage using Violin and Box Plots
+#'
+#' @param win_pct_changes2_neg A dataframe containing negative changes in winning percentages.
+negative_win_pct_change_violin_boxplot <- function(win_pct_changes2_neg) {
+  
+  # Define a color vector
+  color_vector <- viridis::viridis(length(unique(win_pct_changes2_neg$feeder_occupancy_grouped)), direction = -1)
+  
+  # Define labels
+  labels = c("(0.27, 0.43]", "(0.43, 0.6]", "(0.6, 0.77]", "(0.77, 1]")
+  
+  violin_boxplot <- ggplot(win_pct_changes2_neg, aes(x = as.factor(feeder_occupancy_grouped), y = win_pct_change, fill = as.factor(feeder_occupancy_grouped))) +
+    geom_violin(lwd = 1.5, color = NA) +
+    scale_fill_viridis_d(name = "Feeder \nOccupancy", direction = -1, guide = "none") +
+    geom_boxplot(width = 0.1, fill = "white", lwd = 1.5, outlier.shape = 19, outlier.size = 6, outlier.fill = "black") +
+    labs(x = "Feeder Occupancy", y = "Negative Changes in \nPercentage of Winning", fill = "Feeder Occupancy") +
+    theme_classic() +
+    #theme(text = element_text(size = 55), axis.text.x = element_text(size = 40, color = color_vector)) +
+    theme(text = element_text(size = 55), axis.text.x = element_text(size = 40)) +
+    scale_y_continuous(expand = expansion(mult = c(0, .1)), limits = c(-1, 0)) +
+    scale_x_discrete(labels = labels)
+  
+  file_name = here("graphs/negative_win_pct_change_violin_boxplot.png")
+  ggsave(file_name, plot = violin_boxplot, width = 15, height = 13, limitsize = FALSE)
+  
+}
--- src/03- grouped dyadic analysis.R
+++ src/03- grouped dyadic analysis.R
@@ -1,1 +1,57 @@
-#
+repl_master <- replacement_sampled_master[, c("Actor_cow", "Reactor_cow", "end_density")]
+colnames(df = repl_master) <- c("winner", "loser", "feeder_occupancy")
+
+repl_master_grouped <- group_density_buckets(repl_master, "feeder_occupancy")
+interactions_by_dyad_grouped <- calculate_interactions_by_dyad(repl_master_grouped)
+
+# find dyads that show up in all 5 levels of feeder occupancy
+dyads_in_all_levels_grouped <- find_dyads_in_all_levels(interactions_by_dyad_grouped, "dyad_id", "feeder_occupancy_grouped")
+interactions_by_dyad_same_across_all_levels_grouped <- interactions_by_dyad_grouped[which(interactions_by_dyad_grouped$dyad_id %in% dyads_in_all_levels_grouped),]
+dyads_in_all_levels_grouped_num <- length(dyads_in_all_levels_grouped)
+
+
+win_pct_changes <- calculate_win_pct_change_per_dyad(
+  interactions_by_dyad_same_across_all_levels_grouped, 
+  "dyad_id",
+  "feeder_occupancy_grouped", 
+  "win_pct"
+)
+
+# the changes in win pct is all relative to the lowest level of feeder occupancy after grouping (which is 0% - 27%)
+win_pct_changes2 <- win_pct_changes[which(win_pct_changes$feeder_occupancy_grouped != 0.27),]
+win_pct_changes2_0 <- win_pct_changes2[which(win_pct_changes2$win_pct_change ==0),]
+win_pct_changes2_pos <- win_pct_changes2[which(win_pct_changes2$win_pct_change >0),]
+win_pct_changes2_neg <- win_pct_changes2[which(win_pct_changes2$win_pct_change <0),]
+
+# plot dyads with positive and negative changes
+positive_win_pct_change_violin_boxplot(win_pct_changes2_pos)
+negative_win_pct_change_violin_boxplot(win_pct_changes2_neg) 
+
+
+# Fit a mixed-effects model
+win_pct_changes_pos <- lmerTest::lmer(win_pct_change ~ feeder_occupancy_grouped + (1 | dyad_id), data = win_pct_changes2_pos)
+summary(win_pct_changes_pos)
+win_pct_changes_neg <- lmerTest::lmer(win_pct_change ~ feeder_occupancy_grouped + (1 | dyad_id), data = win_pct_changes2_neg)
+summary(win_pct_changes_neg)
+
+# calculate the percentage of dyads with 0 change in win_pct across all levels
+total_same_dyad <- length(unique(win_pct_changes2$dyad_id))
+grouped_df <- dplyr::group_by(win_pct_changes2_0, feeder_occupancy_grouped)
+unique_dyad_counts <- dplyr::summarise(grouped_df, n_unique_dyads = dplyr::n_distinct(dyad_id))
+colnames(unique_dyad_counts) <- c("feeder_occupancy_grouped", "dyads_0_change")
+unique_dyad_counts$dyads_0_change_pct <- unique_dyad_counts$dyads_0_change/total_same_dyad
+
+# calculate the percentage of dyads with negative changes in win pct in win_pct across all levels
+total_same_dyad <- length(unique(win_pct_changes2$dyad_id))
+grouped_df_neg <- dplyr::group_by(win_pct_changes2_neg, feeder_occupancy_grouped)
+unique_dyad_counts_neg <- dplyr::summarise(grouped_df_neg, n_unique_dyads = dplyr::n_distinct(dyad_id))
+colnames(unique_dyad_counts_neg) <- c("feeder_occupancy_grouped", "dyads_neg_change")
+unique_dyad_counts_neg$dyads_neg_change_pct <- unique_dyad_counts_neg$dyads_neg_change / total_same_dyad
+
+# calculate the percentage of dyads with positive changes in win pct in win_pct across all levels
+total_same_dyad <- length(unique(win_pct_changes2$dyad_id))
+grouped_df_pos <- dplyr::group_by(win_pct_changes2_pos, feeder_occupancy_grouped)
+unique_dyad_counts_pos <- dplyr::summarise(grouped_df_pos, n_unique_dyads = dplyr::n_distinct(dyad_id))
+colnames(unique_dyad_counts_pos) <- c("feeder_occupancy_grouped", "dyads_pos_change")
+unique_dyad_counts_pos$dyads_pos_change_pct <- unique_dyad_counts_pos$dyads_pos_change / total_same_dyad
+
