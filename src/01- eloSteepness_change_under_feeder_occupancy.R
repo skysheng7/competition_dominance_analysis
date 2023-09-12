eabee0a64c3b73d021d31fa597b16369c19c588b
Sheng <82303914+skysheng7@users.noreply.github.com>
2023-09-12 13:56:28 -0700

    test

--- src/01- eloSteepness_change_under_feeder_occupancy.R
+++ src/01- eloSteepness_change_under_feeder_occupancy.R
@@ -11,3 +11,4 @@ elo_steepness_plot(master_steepness)
 # fit a linear model for Elo Steepness X Feeder Occupancy
 steepness_lm <- lm(master_steepness$steepness_mean ~ master_steepness$resource_occupancy)
 steepness_lm_summary <- summary(steepness_lm)
+
