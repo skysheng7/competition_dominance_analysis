dc46a713f06c464c83d735558a38b83efeb8ffba
Kehan (Sky) Sheng <82303914+skysheng7@users.noreply.github.com>
2024-02-28 05:18:02 -0800

    Update 01- eloSteepness_change_under_feeder_occupancy.R

--- src/01- eloSteepness_change_under_feeder_occupancy.R
+++ src/01- eloSteepness_change_under_feeder_occupancy.R
@@ -6,9 +6,11 @@ load.project()
 ################################################################################
 # plot the steepness of elo under different Feeder Occupancy
 master_steepness2 <- master_steepness
-elo_steepness_plot(master_steepness)
+master_steepness2$resource_occupancy <- master_steepness2$resource_occupancy * 100
+elo_steepness_plot(master_steepness2)
 
 # fit a linear model for Elo Steepness X Feeder Occupancy
-steepness_lm <- lm(master_steepness$steepness_mean ~ master_steepness$resource_occupancy)
+steepness_lm <- lm(master_steepness2$steepness_mean ~ master_steepness2$resource_occupancy)
 steepness_lm_summary <- summary(steepness_lm)
+
 
